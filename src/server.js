16bb5231ffddfb1e2b79d79661155b3b0d2c67bf
Mahbub049 <mahbubsarwar49@gmail.com>
2026-07-09 02:28:52 +0600

    Allow bubt Firebase hosting domain

--- src/server.js
+++ src/server.js
@@ -52,7 +52,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.options(/.*/, cors(corsOptions));
 
-app.options(/.*/, cors());
+// app.options(/.*/, cors());
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
